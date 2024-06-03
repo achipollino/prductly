73dbd16ca0ebcc6f62c1781145d9c009d7278398
achipollino <alinechipollino@gmail.com>
2024-06-03 16:07:00 +0300

    feat: generate article

---
+++ src/js/Article.js
@@ -0,0 +1,42 @@
+export class Article {
+    constructor({id, title, urlToImage, tags, ...rest }) {
+        this.id = id;
+        this.title = title;
+        this.urlToImage = urlToImage;
+        this.tags = tags;
+    }
+
+    // Article generator
+
+    generateArticle() {
+        let tamplate = '';
+        let article = document.createElement('article');
+        article.className = 'strategy block-shadowed';
+        article.setAttribute('data-id', this.id);
+
+        this.urlToImage &&
+        (tamplate += `<img class="strategy__image" src=${this.urlToImage} alt="strategy">`);
+
+        if (this.title || this.tags) {
+            tamplate += `<div class="strategy__content">`
+
+            this.title &&
+            (tamplate += `<h3 class="strategy__name">${this.title}</h3>`);
+
+            if(this.tags) {
+                tamplate += `<div class="strategy__tags tags">`
+
+                this.tags.map(tag => {
+                    template += `<span class="tag tag_colored">${tag}</span>`
+                })
+
+                tamplate += `</div>`
+            }
+            
+            tamplate += `</div>`
+        }
+
+        article.innerHTML = template;
+        return article;
+    }
+}
